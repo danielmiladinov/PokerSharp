1e4463072a8685a38bbce95246ef43998c8b7c9e
Daniel Miladinov <daniel.miladinov@gmail.com>
2012-08-13 14:36:56 -0400

    StraightSpecification receives newHand().
    StraightSpecification should now be able to return the best
    Straight possible given a Hand with any number of Cards.
    
    The isSatisfiedBy(Hand) method was able to remain unchanged, but I
    had to override _canBeAStraight() here in order to hook newHand()'s
    behavior into into it.  I did it this way since _canBeAStraight()
    is also used elsewhere.

--- HandSpecifications/StraightSpecification.php
+++ HandSpecifications/StraightSpecification.php
@@ -9,4 +9,49 @@ class StraightSpecification extends HandSpecification {
     public function isSatisfiedBy(Hand $Hand) {
         return $this->_canBeAStraight($Hand) && !$this->_canBeAFlush($Hand);
     }
+
+    /**
+     * @param Hand $Hand
+     * @return boolean
+     */
+    protected function _canBeAStraight(Hand $Hand) {
+        return $this->newHand($Hand) instanceof Straight;
+    }
+
+    /**
+     * @param Hand $Hand
+     * @return Straight
+     */
+    public function newHand(Hand $Hand) {
+        $StraightCards = array();
+        $PreviousCard = null;
+
+        $Cards = $Hand->getCards();
+
+        usort(
+            $Cards,
+            function (Card $Card1, Card $Card2) {
+                return $Card1->compareTo($Card2);
+            }
+        );
+
+        foreach ($Cards as $Card) {
+            if ($PreviousCard instanceof Card) {
+                if ($PreviousCard->getFaceValue() - 1 == $Card->getFaceValue()) {
+                    $StraightCards[] = $Card;
+                } else {
+                    $StraightCards = array($Card);
+                }
+            } else {
+                $StraightCards[] = $Card;
+            }
+
+            $PreviousCard = $Card;
+            if (count($StraightCards) == 5) {
+                return new Straight($StraightCards);
+            }
+        }
+
+        return null;
+    }
 }
