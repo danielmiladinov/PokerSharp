f5f5a82fc82a02820a1751fb01f1fb82bb811e0e
Daniel Miladinov <daniel.miladinov@gmail.com>
2012-08-12 23:07:11 -0400

    Changed FullHouseSpecification's mission somewhat.
    Instead of just handling whether a Hand can be a FullHouse, it'll
    take a Hand whose cards could be a FullHouse and return a FullHouse
    without any other cards.  If such a hand could not be made, it instead
    returns null.  To stay compatible with its old function, satisfying the
    specification is a simple matter of running Hands through the newHand
    method and testing what comes out the other end.

--- HandSpecifications/FullHouseSpecification.php
+++ HandSpecifications/FullHouseSpecification.php
@@ -7,21 +7,52 @@ class FullHouseSpecification extends HandSpecification {
      * @return boolean
      */
     public function isSatisfiedBy(Hand $Hand) {
+        return $this->newHand($Hand) instanceof FullHouse;
+    }
+
+    /**
+     * @param Hand $Hand
+     * @return FullHouse
+     */
+    public function newHand(Hand $Hand) {
+        $HandCards = array();
         $GroupedByValue = $Hand->getCardsGroupedByValues();
         $faceValueCounts = array();
 
         foreach ($GroupedByValue as $faceValue => $CardsWithSameValue) {
-            $faceValueCounts[$faceValue] = count($CardsWithSameValue);
+            $faceValueCount = count($CardsWithSameValue);
+
+            if ($faceValueCount >= 2) {
+                $faceValueCounts[$faceValue] = $faceValueCount;
+            }
         }
 
-        asort($faceValueCounts);
-        $highestCount = array_pop($faceValueCounts);
+        if (count($faceValueCounts) >= 2) {
+            krsort($faceValueCounts);
 
-        if ($highestCount == 3) {
-            $nextHighestCount = array_pop($faceValueCounts);
-            return $nextHighestCount == 2;
-        } else {
-            return false;
+            list($faceValue, $faceValueCount) = each($faceValueCounts);
+
+            if ($faceValueCount == 3) {
+                $HandCards = array_merge($HandCards, $GroupedByValue[$faceValue]);
+
+                list($nextFaceValue, $nextFaceValueCount) = each($faceValueCounts);
+
+                if ($nextFaceValueCount >= 2) {
+                    $HandCards = array_merge($HandCards, array_slice($GroupedByValue[$nextFaceValue], 0, 2));
+                    return new FullHouse($HandCards);
+                }
+            } else if ($faceValueCount == 2) {
+                $HandCards = array_merge($HandCards, $GroupedByValue[$faceValue]);
+
+                list($nextFaceValue, $nextFaceValueCount) = each($faceValueCounts);
+
+                if ($nextFaceValueCount == 3) {
+                    $HandCards = array_merge($HandCards, array_slice($GroupedByValue[$nextFaceValue], 0, 3));
+                    return new FullHouse($HandCards);
+                }
+            }
         }
+
+        return null;
     }
 }
