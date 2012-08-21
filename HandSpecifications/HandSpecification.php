76af72442e40d4c975f7de8325af342f0c0c85a5
Daniel Miladinov <daniel.miladinov@gmail.com>
2012-08-21 13:59:59 -0400

    Let newHand() also be an abstract method of HandSpecification.
    Now that every HandSpecification already has an implementation, we might
    as well bake that into the interface contract.

--- HandSpecifications/HandSpecification.php
+++ HandSpecifications/HandSpecification.php
@@ -8,4 +8,11 @@ abstract class HandSpecification {
      * @return boolean
      */
     public abstract function isSatisfiedBy(Hand $Hand);
+
+    /**
+     * @abstract
+     * @param Hand $Hand
+     * @return Hand
+     */
+    public abstract function newHand(Hand $Hand);
 }
