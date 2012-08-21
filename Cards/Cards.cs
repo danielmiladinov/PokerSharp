6f588a1c90b32b6b1c6230ae7567e0ff1552bafd
Daniel Miladinov <daniel.miladinov@gmail.com>
2012-08-21 19:25:46 -0400

    Convert the Cards class from php to C#.
    And already, there's just a hint of the fun challenges that yet lay
    ahead! Php's array data structure (and the loose typing) are just about
    the only things from the language that I'll be missing...

--- Cards/Cards.cs
+++ Cards/Cards.cs
@@ -1,127 +1,73 @@
-<?php
+using System.Collections.Generic;
 
 /**
  * A handy container for getting a particular face value of a given suit.
  */
 class Cards {
-    /**
-     * @static
-     * @param Suit $Suit
-     * @return Card
-     */
-    public static function aceOf(Suit $Suit) {
-        return $Suit->getCard(Card::ACE);
+    
+    public static Card aceOf(Suit Suit) {
+        return Suit.getCard(Card::ACE);
     }
 
-    /**
-     * @static
-     * @param Suit $Suit
-     * @return Card
-     */
-    public static function twoOf(Suit $Suit) {
-        return $Suit->getCard(2);
+    public static Card twoOf(Suit Suit) {
+        return Suit.getCard(2);
     }
 
-    /**
-     * @param Suit $Suit
-     * @return Card
-     */
-    public static function threeOf(Suit $Suit) {
-        return $Suit->getCard(3);
+    public static Card threeOf(Suit Suit) {
+        return Suit.getCard(3);
     }
 
-    /**
-     * @param Suit $Suit
-     * @return Card
-     */
-    public static function fourOf(Suit $Suit) {
-        return $Suit->getCard(4);
+    public static Card fourOf(Suit Suit) {
+        return Suit.getCard(4);
     }
 
-    /**
-     * @param Suit $Suit
-     * @return Card
-     */
-    public static function fiveOf(Suit $Suit) {
-        return $Suit->getCard(5);
+    public static function fiveOf(Suit Suit) {
+        return Suit.getCard(5);
     }
 
-    /**
-     * @param Suit $Suit
-     * @return Card
-     */
-    public static function sixOf(Suit $Suit) {
-        return $Suit->getCard(6);
+    public static function sixOf(Suit Suit) {
+        return Suit.getCard(6);
     }
 
-    /**
-     * @param Suit $Suit
-     * @return Card
-     */
-    public static function sevenOf(Suit $Suit) {
-        return $Suit->getCard(7);
+    public static function sevenOf(Suit Suit) {
+        return Suit.getCard(7);
     }
 
-    /**
-     * @param Suit $Suit
-     * @return Card
-     */
-    public static function eightOf(Suit $Suit) {
-        return $Suit->getCard(8);
+    public static function eightOf(Suit Suit) {
+        return Suit.getCard(8);
     }
 
-    /**
-     * @param Suit $Suit
-     * @return Card
-     */
-    public static function nineOf(Suit $Suit) {
-        return $Suit->getCard(9);
+    public static function nineOf(Suit Suit) {
+        return Suit.getCard(9);
     }
 
-    /**
-     * @param Suit $Suit
-     * @return Card
-     */
-    public static function tenOf(Suit $Suit) {
-        return $Suit->getCard(10);
+    public static function tenOf(Suit Suit) {
+        return Suit.getCard(10);
     }
 
-    /**
-     * @param Suit $Suit
-     * @return Card
-     */
-    public static function jackOf(Suit $Suit) {
-        return $Suit->getCard(Card::JACK);
+    public static function jackOf(Suit Suit) {
+        return Suit.getCard(Card::JACK);
     }
 
-    /**
-     * @param Suit $Suit
-     * @return Card
-     */
-    public static function queenOf(Suit $Suit) {
-        return $Suit->getCard(Card::QUEEN);
+    public static function queenOf(Suit Suit) {
+        return Suit.getCard(Card::QUEEN);
     }
 
-    /**
-     * @param Suit $Suit
-     * @return Card
-     */
-    public static function kingOf(Suit $Suit) {
-        return $Suit->getCard(Card::KING);
+    public static function kingOf(Suit Suit) {
+        return Suit.getCard(Card::KING);
     }
 
-    /**
-     * @static
-     * @param \Card[] $Cards
-     * @return \Card[]
-     */
-    public static function getCardsGroupedByValue(array $Cards) {
-        $CardsGroupedByValues = array();
+    public static Dictionary<int, List<Card>> getCardsGroupedByValue(List<Card> Cards) {
+        var CardsGroupedByValues = new Dictionary<int, List<Card>>();
 
-        foreach ($Cards as $Card) {
-            $CardsGroupedByValues[$Card->getFaceValue()][] = $Card;
+        foreach (var Card in Cards) {
+            if (! CardsGroupedByValues.ContainsKey(Card.getFaceValue())) {
+                CardsGroupedByValues.Add(Card.getFaceValue(), new List<Card>());
+            }
+
+            CardsGroupedByValues.TryGetValue(Card.getFaceValue()).Add(Card);
         }
 
-        return $CardsGroupedByValues;
+        return CardsGroupedByValues;
     }
 }
