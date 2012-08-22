6e3f8792d89bd61af6d90c79240252144fb16829
Daniel Miladinov <daniel.miladinov@gmail.com>
2012-08-22 19:09:08 -0400

    Convert CardBuilderTest from php to C#.
    Will it build? Who knows? I'm still just going through the code, file by
    file, doing my best to faithfully preserve the original intent of the
    php code by representing it as best as possible, to my knowledge, in as
    close to equivalent C# as I can.

--- tests/CardBuilderTest.cs
+++ tests/CardBuilderTest.cs
@@ -1,125 +1,92 @@
-<?php
+using System;
+using NUnit.Framework;
 
-class CardBuilderTest extends PHPUnit_Framework_TestCase {
-    /**
-     * @var CardBuilder
-     */
-    private $_CardBuilder;
+[TestFixture]
+class CardBuilderTest {
 
-    public function setUp() {
-        $this->_CardBuilder = new CardBuilder();
+    private CardBuilder CardBuilder;
+
+    [SetUp]
+    public void setUp() {
+        CardBuilder = new CardBuilder();
     }
 
-    /**
-     * @test
-     * @return void
-     */
-    public function willThrowAnExceptionFromInvalidString() {
-        $invalidString = 'aa';
-
-        $this->setExpectedException('CardBuilderException', 'Invalid card string');
-
-        $this->_CardBuilder->fromString($invalidString);
+    [Test]
+    [ExpectedException(typeof(CardBuilderException))]
+    public void willThrowAnExceptionFromInvalidString() {
+        var invalidString = "aa";
+        CardBuilder.fromString(invalidString);
     }
 
-    /**
-     * @test
-     * @return void
-     */
-    public function willThrowAnExceptionFromInvalidNumberTooHigh() {
-        $invalidString = '15-C';
 
-        $this->setExpectedException('CardBuilderException', 'Invalid face value');
-
-        $this->_CardBuilder->fromString($invalidString);
+    [Test]
+    [ExpectedException(typeof(CardBuilderException))]
+    public void willThrowAnExceptionFromInvalidNumberTooHigh() {
+        var invalidString = "15-C";
+        CardBuilder.fromString(invalidString);
     }
 
-    /**
-     * @test
-     * @return void
-     */
-    public function willThrowAnExceptionFromInvalidNumberTooLow() {
-        $invalidString = '1-C';
-
-        $this->setExpectedException('CardBuilderException', 'Invalid face value');
-
-        $this->_CardBuilder->fromString($invalidString);
+    [Test]
+    [ExpectedException(typeof(CardBuilderException))]
+    public void willThrowAnExceptionFromInvalidNumberTooLow() {
+        var invalidString = "1-C";
+        CardBuilder.fromString(invalidString);
     }
 
-    /**
-     * @test
-     * @return void
-     */
-    public function willThrowAnExceptionFromInvalidSuit() {
-        $invalidString = '2-Z';
-
-        $this->setExpectedException('CardBuilderException', 'Invalid suit');
-
-        $this->_CardBuilder->fromString($invalidString);
+    [Test]
+    [ExpectedException(typeof(CardBuilderException))]
+    public void willThrowAnExceptionFromInvalidSuit() {
+        var invalidString = "2-Z";
+        CardBuilder.fromString(invalidString);
     }
 
-    /**
-     * @test
-     * @return void
-     */
-    public function willGetACardWithValidParameters() {
-        $validString = '2-C';
-        $ExpectedCard = new Clubs(2);
+    [Test]
+    public void willGetACardWithValidParameters() {
+        var validString = "2-C";
+        ExpectedCard = new Clubs(2);
 
-        $ActualCard = $this->_CardBuilder->fromString($validString);
+        ActualCard = CardBuilder.fromString(validString);
 
-        $this->assertEquals($ExpectedCard, $ActualCard);
+        Assert.AreEqual(ExpectedCard, ActualCard);
     }
 
-    /**
-     * @test
-     * @return void
-     */
-    public function willGetACardWithFaceCard() {
-        $validString = 'Q-S';
-        $ExpectedCard = new Spades(Card::QUEEN);
+    [Test]
+    public void willGetACardWithFaceCard() {
+        var validString = "Q-S";
+        ExpectedCard = new Spades(Card::QUEEN);
 
-        $ActualCard = $this->_CardBuilder->fromString($validString);
+        ActualCard = CardBuilder.fromString(validString);
 
-        $this->assertEquals($ExpectedCard, $ActualCard);
+        Assert.AreEqual(ExpectedCard, ActualCard);
     }
 
-    /**
-     * @test
-     * @return void
-     */
-    public function willGetACardWithJacks() {
-        $validString = 'J-H';
-        $ExpectedCard = new Hearts(Card::JACK);
+    [Test]
+    public void willGetACardWithJacks() {
+        var validString = "J-H";
+        ExpectedCard = new Hearts(Card::JACK);
 
-        $ActualCard = $this->_CardBuilder->fromString($validString);
+        ActualCard = CardBuilder.fromString(validString);
 
-        $this->assertEquals($ExpectedCard, $ActualCard);
+        Assert.AreEqual(ExpectedCard, ActualCard);
     }
 
-    /**
-     * @test
-     * @return void
-     */
-    public function willGetACardWithKings() {
-        $validString = 'K-D';
-        $ExpectedCard = new Diamonds(Card::KING);
+    [Test]
+    public void willGetACardWithKings() {
+        var validString = "K-D";
+        ExpectedCard = new Diamonds(Card::KING);
 
-        $ActualCard = $this->_CardBuilder->fromString($validString);
+        ActualCard = CardBuilder.fromString(validString);
 
-        $this->assertEquals($ExpectedCard, $ActualCard);
+        Assert.AreEqual(ExpectedCard, ActualCard);
     }
 
-    /**
-     * @test
-     * @return void
-     */
-    public function willGetACardWithAce() {
-        $validString = 'A-C';
-        $ExpectedCard = new Clubs(Card::ACE);
+    [Test]
+    public void willGetACardWithAce() {
+        var validString = "A-C";
+        ExpectedCard = new Clubs(Card::ACE);
 
-        $ActualCard = $this->_CardBuilder->fromString($validString);
+        ActualCard = CardBuilder.fromString(validString);
 
-        $this->assertEquals($ExpectedCard, $ActualCard);
+        Assert.AreEqual(ExpectedCard, ActualCard);
     }
 }
