cad6d1ab966edb07e7b4558c4201e0d8beaee064
Daniel Miladinov <daniel.miladinov@gmail.com>
2012-08-23 19:45:00 -0400

    Convert Deck from php to C#.

--- Deck.cs
+++ Deck.cs
@@ -1,66 +1,51 @@
-<?php
+using System.Collections.Generic;
 
 class Deck {
-    /**
-     * @var Card[]
-     */
-    private $_Cards;
 
-    public function __construct() {
-        $this->_Cards = array();
+    private List<Card> Cards;
+
+    public Deck() {
+        Cards = new List<Card>();
     }
 
-    /**
-     * @param Card $Card
-     * @throws DeckIntegrityException
-     */
-    public function add(Card $Card) {
-        if (array_key_exists("{$Card}", $this->_Cards)) {
-            throw new DeckIntegrityException('Cannot contain the same card twice!');
+    public void add(Card Card) {
+        if (Cards.Contains(Card)) {
+            throw new DeckIntegrityException("Cannot contain the same card twice!");
         } else {
-            $this->_Cards["{$Card}"] = $Card;
+            Cards.Add(Card);
         }
     }
 
-    /**
-     * @return int
-     */
-    public function size() {
-        return count($this->_Cards);
+    public int size() {
+        return Cards.Length;
     }
 
-    /**
-     * @return void
-     */
-    public function populate() {
-        $Suits = array(
+    public void populate() {
+        var Suits = new Suit[] {
             Suit::Spades(),
             Suit::Hearts(),
             Suit::Clubs(),
             Suit::Diamonds(),
-        );
+        };
 
-        foreach ($Suits as $Suit) {
-            $this->_Cards[] = Cards::aceOf($Suit);
-            $this->_Cards[] = Cards::twoOf($Suit);
-            $this->_Cards[] = Cards::threeOf($Suit);
-            $this->_Cards[] = Cards::fourOf($Suit);
-            $this->_Cards[] = Cards::fiveOf($Suit);
-            $this->_Cards[] = Cards::sixOf($Suit);
-            $this->_Cards[] = Cards::sevenOf($Suit);
-            $this->_Cards[] = Cards::eightOf($Suit);
-            $this->_Cards[] = Cards::nineOf($Suit);
-            $this->_Cards[] = Cards::tenOf($Suit);
-            $this->_Cards[] = Cards::jackOf($Suit);
-            $this->_Cards[] = Cards::queenOf($Suit);
-            $this->_Cards[] = Cards::kingOf($Suit);
+        foreach (var Suit in Suits) {
+            Cards.Add(Cards::aceOf(Suit));
+            Cards.Add(Cards::twoOf(Suit));
+            Cards.Add(Cards::threeOf(Suit));
+            Cards.Add(Cards::fourOf(Suit));
+            Cards.Add(Cards::fiveOf(Suit));
+            Cards.Add(Cards::sixOf(Suit));
+            Cards.Add(Cards::sevenOf(Suit));
+            Cards.Add(Cards::eightOf(Suit));
+            Cards.Add(Cards::nineOf(Suit));
+            Cards.Add(Cards::tenOf(Suit));
+            Cards.Add(Cards::jackOf(Suit));
+            Cards.Add(Cards::queenOf(Suit));
+            Cards.Add(Cards::kingOf(Suit));
         }
     }
 
-    /**
-     * @return Card[]
-     */
-    public function getCards() {
-        return array_values($this->_Cards);
+    public Card[] getCards() {
+        return Cards.ToArray();
     }
 }
