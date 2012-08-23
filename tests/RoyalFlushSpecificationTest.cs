338d252f26e92ca05a4c028dde493572f9e361b4
Daniel Miladinov <daniel.miladinov@gmail.com>
2012-08-23 15:04:31 -0400

    Convert RoyalFlushSpecificationTest from php to C#.
    This one was refreshingly easier to conver than what I've had so far;
    none of my zany array closure gymnastics that don't directly map to the
    new language.

--- tests/RoyalFlushSpecificationTest.cs
+++ tests/RoyalFlushSpecificationTest.cs
@@ -1,49 +1,36 @@
-<?php
+using NUnit.Framework;
 
-class RoyalFlushSpecificationTest extends PokerTestCase {
-    /**
-     * @var RoyalFlushSpecification
-     */
-    private $_Specification;
+[TestFixture]
+class RoyalFlushSpecificationTest : PokerTestCase {
 
+    private RoyalFlushSpecification Specification;
 
-    protected function setUp() {
-        $this->_Specification = new RoyalFlushSpecification();
+    [Setup]
+    protected void setUp() {
+        Specification = new RoyalFlushSpecification();
     }
 
-    /**
-     * @test
-     * @return void
-     */
-    public function shouldBeSatisfiedByASpadesRoyalFlush() {
-        $Hand = new Hand($this->_theFiveCardsAre('A-S', 'K-S', 'Q-S', 'J-S', '10-S'));
-        $this->assertTrue($this->_Specification->isSatisfiedBy($Hand), 'This is a valid RoyalFlush, why did it not satisfy the specification?');
+    [Test]
+    public void shouldBeSatisfiedByASpadesRoyalFlush() {
+        Hand = new Hand(theFiveCardsAre("A-S", "K-S", "Q-S", "J-S", "10-S"));
+        Assert.IsTrue(Specification.isSatisfiedBy(Hand), "This is a valid RoyalFlush, why did it not satisfy the specification?");
     }
 
-    /**
-     * @test
-     * @return void
-     */
-    public function shouldNotBeSatisfiedByAStraightFlush() {
-        $Hand = new Hand($this->_theFiveCardsAre('K-S', 'Q-S', 'J-S', '10-S', '9-S'));
-        $this->assertFalse($this->_Specification->isSatisfiedBy($Hand), 'Not just any StraightFlush can be a RoyalFlush!');
+    [Test]
+    public void shouldNotBeSatisfiedByAStraightFlush() {
+        Hand = new Hand(theFiveCardsAre("K-S", "Q-S", "J-S", "10-S", "9-S"));
+        Assert.IsFalse(Specification.isSatisfiedBy(Hand), "Not just any StraightFlush can be a RoyalFlush!");
     }
 
-    /**
-     * @test
-     * @return void
-     */
-    public function shouldNotBeSatisfiedByAnAceHighCard() {
-        $Hand = new Hand($this->_theFiveCardsAre('A-H', '2-S', '3-C', 'J-D', '7-D'));
-        $this->assertFalse($this->_Specification->isSatisfiedBy($Hand), 'Not just any Ace HighCard can be a RoyalFlush!');
+    [Test]
+    public void shouldNotBeSatisfiedByAnAceHighCard() {
+        Hand = new Hand(theFiveCardsAre("A-H", "2-S", "3-C", "J-D", "7-D"));
+        Assert.IsFalse(Specification.isSatisfiedBy(Hand), "Not just any Ace HighCard can be a RoyalFlush!");
     }
 
-    /**
-     * @test
-     * @return void
-     */
-    public function shouldNotBeSatisfiedByAnAceHighNonFlushStraight() {
-        $Hand = new Hand($this->_theFiveCardsAre('A-H', 'K-H', 'Q-C', 'J-H', '10-H'));
-        $this->assertFalse($this->_Specification->isSatisfiedBy($Hand), 'Not just any Ace high Straight can be a RoyalFlush!');
+    [Test]
+    public void shouldNotBeSatisfiedByAnAceHighNonFlushStraight() {
+        Hand = new Hand(theFiveCardsAre("A-H", "K-H", "Q-C", "J-H", "10-H"));
+        Assert.IsFalse(Specification.isSatisfiedBy(Hand), "Not just any Ace high Straight can be a RoyalFlush!");
     }
 }
