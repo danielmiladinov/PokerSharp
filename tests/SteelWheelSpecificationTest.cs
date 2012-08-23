d1b165113b4e235140b6bb2ee5e9d18fb859e519
Daniel Miladinov <daniel.miladinov@gmail.com>
2012-08-23 15:28:10 -0400

    Convert SteelWheelSpecificationTest from php to C#.
    Ahhh, another relatively easy conversion...

--- tests/SteelWheelSpecificationTest.cs
+++ tests/SteelWheelSpecificationTest.cs
@@ -1,49 +1,36 @@
-<?php
+using NUnit.Framework;
 
-class SteelWheelSpecificationTest extends PokerTestCase {
+[TestFixture]
+class SteelWheelSpecificationTest : PokerTestCase {
 
-    /**
-     * @var SteelWheelSpecification
-     */
-    private $_Specification;
+    private SteelWheelSpecification Specification;
 
+    [Setup]
     protected function setUp() {
-        $this->_Specification = new SteelWheelSpecification();
+        Specification = new SteelWheelSpecification();
     }
 
-    /**
-     * @test
-     * @return void
-     */
+    [Test]
     public function shouldBeAbleToIdentifyAClubsSteelWheel() {
-        $Hand = new Hand($this->_theFiveCardsAre('5-C', '4-C', '3-C', '2-C', 'A-C'));
-        $this->assertTrue($this->_Specification->isSatisfiedBy($Hand), 'This is a valid SteelWheel, why did it not satisfy the specification?');
+        Hand = new Hand(theFiveCardsAre("5-C", "4-C", "3-C", "2-C", "A-C"));
+        Assert.IsTrue(Specification.isSatisfiedBy(Hand), "This is a valid SteelWheel, why did it not satisfy the specification?");
     }
 
-    /**
-     * @test
-     * @return void
-     */
+    [Test]
     public function shouldNotBeSatisfiedByJustAFlush() {
-        $Hand = new Hand($this->_theFiveCardsAre('Q-C', '6-C', '10-C', '9-C', '8-C'));
-        $this->assertFalse($this->_Specification->isSatisfiedBy($Hand), 'Not just any Flush can be a SteelWheel!');
+        Hand = new Hand(theFiveCardsAre("Q-C", "6-C", "10-C", "9-C", "8-C"));
+        Assert.IsFalse(Specification.isSatisfiedBy(Hand), "Not just any Flush can be a SteelWheel!");
     }
 
-    /**
-     * @test
-     * @return void
-     */
+    [Test]
     public function shouldNotBeSatisfiedByJustAStraight() {
-        $Hand = new Hand($this->_theFiveCardsAre('5-C', '4-C', '3-C', '2-C', 'A-D'));
-        $this->assertFalse($this->_Specification->isSatisfiedBy($Hand), 'Not just any Straight can be a SteelWheel!');
+        Hand = new Hand(theFiveCardsAre("5-C", "4-C", "3-C", "2-C", "A-D"));
+        Assert.IsFalse(Specification.isSatisfiedBy(Hand), "Not just any Straight can be a SteelWheel!");
     }
 
-    /**
-     * @test
-     * @return void
-     */
+    [Test]
     public function shouldNotBeSatisfiedByJustAStraightFlush() {
-        $Hand = new Hand($this->_theFiveCardsAre('J-C', '10-C', '9-C', '8-C', '7-C'));
-        $this->assertFalse($this->_Specification->isSatisfiedBy($Hand), 'Not just any StraightFlush can be a SteelWheel!');
+        Hand = new Hand(theFiveCardsAre("J-C", "10-C", "9-C", "8-C", "7-C"));
+        Assert.IsFalse(Specification.isSatisfiedBy(Hand), "Not just any StraightFlush can be a SteelWheel!");
     }
 }
