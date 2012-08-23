854350af61fadd8e008c394910b704876ba3b4bd
Daniel Miladinov <daniel.miladinov@gmail.com>
2012-08-23 15:53:44 -0400

    Convert TwoOfAKindSpecificationTest from php to C#.
    Yawn.  We interrupt this commit message to bring you an important public
    service announcement:
    
    VIM IS TEH AWESOMEZ!!!!

--- tests/TwoOfAKindSpecificationTest.cs
+++ tests/TwoOfAKindSpecificationTest.cs
@@ -1,49 +1,36 @@
-<?php
+using NUnit.Framework;
 
-class TwoOfAKindSpecificationTest extends PokerTestCase {
+[TestFixture]
+class TwoOfAKindSpecificationTest : PokerTestCase {
 
-    /**
-     * @var TwoOfAKindSpecification
-     */
-    private $_Specification;
+    private TwoOfAKindSpecification Specification;
 
-    protected function setUp() {
-        $this->_Specification = new TwoOfAKindSpecification();
+    [Setup]
+    protected void setUp() {
+        Specification = new TwoOfAKindSpecification();
     }
 
-    /**
-     * @test
-     * @return void
-     */
-    public function shouldBeSatisfiedByATwoOfAKind() {
-        $Hand = new Hand($this->_theFiveCardsAre('2-C', '3-C', '4-C', '5-C', '4-H'));
-        $this->assertTrue($this->_Specification->isSatisfiedBy($Hand), 'This is a valid TwoOfAKind, why did it not satisfy the specification?');
+    [Test]
+    public void shouldBeSatisfiedByATwoOfAKind() {
+        Hand = new Hand(theFiveCardsAre("2-C", "3-C", "4-C", "5-C", "4-H"));
+        Assert.IsTrue(Specification.isSatisfiedBy(Hand), "This is a valid TwoOfAKind, why did it not satisfy the specification?");
     }
 
-    /**
-     * @test
-     * @return void
-     */
-    public function shouldNotBeSatisfiedByRandomJunkCards() {
-        $Hand = new Hand($this->_theFiveCardsAre('K-H', '10-S', '3-C', '5-D', '7-S'));
-        $this->assertFalse($this->_Specification->isSatisfiedBy($Hand), 'This is not a valid TwoOfAKind!');
+    [Test]
+    public void shouldNotBeSatisfiedByRandomJunkCards() {
+        Hand = new Hand(theFiveCardsAre("K-H", "10-S", "3-C", "5-D", "7-S"));
+        Assert.IsFalse(Specification.isSatisfiedBy(Hand), "This is not a valid TwoOfAKind!");
     }
 
-    /**
-     * @test
-     * @return void
-     */
-    public function shouldNotBeSatisfiedByAThreeOfAKind() {
-        $Hand = new Hand($this->_theFiveCardsAre('K-H', 'K-S', 'K-C', '5-D', '7-S'));
-        $this->assertFalse($this->_Specification->isSatisfiedBy($Hand), 'This is not a valid TwoOfAKind!');
+    [Test]
+    public void shouldNotBeSatisfiedByAThreeOfAKind() {
+        Hand = new Hand(theFiveCardsAre("K-H", "K-S", "K-C", "5-D", "7-S"));
+        Assert.IsFalse(Specification.isSatisfiedBy(Hand), "This is not a valid TwoOfAKind!");
     }
 
-    /**
-     * @test
-     * @return void
-     */
-    public function shouldNotBeSatisfiedByAFourOfAKind() {
-        $Hand = new Hand($this->_theFiveCardsAre('K-H', 'K-S', 'K-C', 'K-D', '7-S'));
-        $this->assertFalse($this->_Specification->isSatisfiedBy($Hand), 'This is not a valid TwoOfAKind!');
+    [Test]
+    public void shouldNotBeSatisfiedByAFourOfAKind() {
+        Hand = new Hand(theFiveCardsAre("K-H", "K-S", "K-C", "K-D", "7-S"));
+        Assert.IsFalse(Specification.isSatisfiedBy(Hand), "This is not a valid TwoOfAKind!");
     }
 }
