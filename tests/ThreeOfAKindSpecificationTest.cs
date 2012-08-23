82e94bdb4541a92c5dd5bbeb0b87fe53d38ea4fe
Daniel Miladinov <daniel.miladinov@gmail.com>
2012-08-23 15:49:17 -0400

    Convert ThreeOfAKindSpecificationTest from php to C#.
    How many more tests to go?

--- tests/ThreeOfAKindSpecificationTest.cs
+++ tests/ThreeOfAKindSpecificationTest.cs
@@ -1,31 +1,24 @@
-<?php
+using NUnit.Framework;
 
-class ThreeOfAKindSpecificationTest extends PokerTestCase {
+[TestFixture]
+class ThreeOfAKindSpecificationTest : PokerTestCase {
 
-    /**
-     * @var ThreeOfAKindSpecification
-     */
-    private $_Specification;
+    private ThreeOfAKindSpecification Specification;
 
-    protected function setUp() {
-        $this->_Specification = new ThreeOfAKindSpecification();
+    [Setup]
+    protected void setUp() {
+        Specification = new ThreeOfAKindSpecification();
     }
 
-    /**
-     * @test
-     * @return void
-     */
-    public function shouldBeSatisfiedByAThreeOfAKind() {
-        $Hand = new Hand($this->_theFiveCardsAre('J-H', 'J-C', 'J-D', '3-S', 'K-H'));
-        $this->assertTrue($this->_Specification->isSatisfiedBy($Hand), 'This is a valid ThreeOfAKind, why did it not satisfy the specification?');
+    [Test]
+    public void shouldBeSatisfiedByAThreeOfAKind() {
+        Hand = new Hand(theFiveCardsAre("J-H", "J-C", "J-D", "3-S", "K-H"));
+        Assert.IsTrue(Specification.isSatisfiedBy(Hand), "This is a valid ThreeOfAKind, why did it not satisfy the specification?");
     }
 
-    /**
-     * @test
-     * @return void
-     */
-    public function shouldNotBeSatisfiedByATwoOfAKind() {
-        $Hand = new Hand($this->_theFiveCardsAre('J-H', 'Q-C', 'J-D', '3-S', 'K-H'));
-        $this->assertFalse($this->_Specification->isSatisfiedBy($Hand), 'This is not a ThreeOfAKind!');
+    [Test]
+    public void shouldNotBeSatisfiedByATwoOfAKind() {
+        Hand = new Hand(theFiveCardsAre("J-H", "Q-C", "J-D", "3-S", "K-H"));
+        Assert.IsFalse(Specification.isSatisfiedBy(Hand), "This is not a ThreeOfAKind!");
     }
 }
