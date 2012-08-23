a8a1d83420080ebcbf4348dee324c20419302e09
Daniel Miladinov <daniel.miladinov@gmail.com>
2012-08-23 15:29:07 -0400

    Convert StraightFlushSpecificationTest from php to C#.
    Another easy conversion in the bag!

--- tests/StraightFlushSpecificationTest.cs
+++ tests/StraightFlushSpecificationTest.cs
@@ -1,40 +1,30 @@
-<?php
+using NUnit.Framework;
 
-class StraightFlushSpecificationTest extends PokerTestCase {
+[TestFixture]
+class StraightFlushSpecificationTest : PokerTestCase {
 
-    /**
-     * @var StraightFlushSpecification
-     */
-    private $_Specification;
+    private StraightFlushSpecification Specification;
 
+    [Setup]
     protected function setUp() {
-        $this->_Specification = new StraightFlushSpecification();
+        Specification = new StraightFlushSpecification();
     }
 
-    /**
-     * @test
-     * @return void
-     */
+    [Test]
     public function shouldBeSatisfiedByAHeartsStraightFlush() {
-        $Hand = new Hand($this->_theFiveCardsAre('Q-H', 'J-H', '10-H', '9-H', '8-H'));
-        $this->assertTrue($this->_Specification->isSatisfiedBy($Hand), 'This is a valid StraightFlush, why did it not satisfy the specification?');
+        Hand = new Hand(theFiveCardsAre("Q-H", "J-H", "10-H", "9-H", "8-H"));
+        Assert.IsTrue(Specification.isSatisfiedBy(Hand), "This is a valid StraightFlush, why did it not satisfy the specification?");
     }
 
-    /**
-     * @test
-     * @return void
-     */
+    [Test]
     public function shouldNotBeSatisfiedByJustAFlush() {
-        $Hand = new Hand($this->_theFiveCardsAre('Q-C', '6-C', '10-C', '9-C', '8-C'));
-        $this->assertFalse($this->_Specification->isSatisfiedBy($Hand), 'Not just any Flush can be a StraightFlush!');
+        Hand = new Hand(theFiveCardsAre("Q-C", "6-C", "10-C", "9-C", "8-C"));
+        Assert.IsFalse(Specification.isSatisfiedBy(Hand), "Not just any Flush can be a StraightFlush!");
     }
 
-    /**
-     * @test
-     * @return void
-     */
+    [Test]
     public function shouldNotBeSatisfiedByJustAStraight() {
-        $Hand = new Hand($this->_theFiveCardsAre('Q-H', 'J-C', '10-D', '9-S', '8-H'));
-        $this->assertFalse($this->_Specification->isSatisfiedBy($Hand), 'Not just any Straight can be a StraightFlush!');
+        Hand = new Hand(theFiveCardsAre("Q-H", "J-C", "10-D", "9-S", "8-H"));
+        Assert.IsFalse(Specification.isSatisfiedBy(Hand), "Not just any Straight can be a StraightFlush!");
     }
 }
