1fe26aaece810e0375b0b3122f929cb827546055
Daniel Miladinov <daniel.miladinov@gmail.com>
2012-08-23 16:44:00 -0400

    Convert TwoPairSpecificationTest from php to C#.
    Yep.

--- tests/TwoPairSpecificationTest.cs
+++ tests/TwoPairSpecificationTest.cs
@@ -1,31 +1,24 @@
-<?php
+using NUnit.Framework;
 
-class TwoPairSpecificationTest extends PokerTestCase {
+[TestFixture]
+class TwoPairSpecificationTest : PokerTestCase {
 
-    /**
-     * @var TwoPairSpecification
-     */
-    private $_Specification;
+    private TwoPairSpecification Specification;
 
-    protected function setUp() {
-        $this->_Specification = new TwoPairSpecification();
+    [Setup]
+    protected void setUp() {
+        Specification = new TwoPairSpecification();
     }
 
-    /**
-     * @test
-     * @return void
-     */
-    public function shouldBeSatisfiedByATwoPair() {
-        $Hand = new Hand($this->_theFiveCardsAre('A-S', '9-C', '9-D', '3-C', '3-H'));
-        $this->assertTrue($this->_Specification->isSatisfiedBy($Hand), 'This is a valid TwoPair, why did it not satisfy the specification?');
+    [Test]
+    public void shouldBeSatisfiedByATwoPair() {
+        Hand = new Hand(theFiveCardsAre("A-S", "9-C", "9-D", "3-C", "3-H"));
+        Assert.IsTrue(Specification.isSatisfiedBy(Hand), "This is a valid TwoPair, why did it not satisfy the specification?");
     }
 
-    /**
-     * @test
-     * @return void
-     */
-    public function shouldNotBeSatisfiedByAFullHouse() {
-        $Hand = new Hand($this->_theFiveCardsAre('3-H', '3-D', '3-S', 'K-C', 'K-D'));
-        $this->assertFalse($this->_Specification->isSatisfiedBy($Hand), 'This is not a valid TwoPair!');
+    [Test]
+    public void shouldNotBeSatisfiedByAFullHouse() {
+        Hand = new Hand(theFiveCardsAre("3-H", "3-D", "3-S", "K-C", "K-D"));
+        Assert.IsFalse(Specification.isSatisfiedBy(Hand), "This is not a valid TwoPair!");
     }
 }
