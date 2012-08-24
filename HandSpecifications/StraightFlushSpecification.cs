a5322d42cdbea12e9a97b8d2d08b09830e0b94a6
Daniel Miladinov <daniel.miladinov@gmail.com>
2012-08-24 15:42:19 -0400

    Convert StraightFlushSpecification from php to C#.

--- HandSpecifications/StraightFlushSpecification.cs
+++ HandSpecifications/StraightFlushSpecification.cs
@@ -1,68 +1,51 @@
-<?php
+class StraightFlushSpecification : HandSpecification {
 
-class StraightFlushSpecification extends HandSpecification {
-
-    /**
-     * @param Hand $Hand
-     * @return boolean
-     */
-    public function isSatisfiedBy(Hand $Hand) {
-        return $this->newHand($Hand) instanceof StraightFlush;
+    public override bool isSatisfiedBy(Hand Hand) {
+        return newHand(Hand) is StraightFlush;
     }
 
-    /**
-     * @param Hand $Hand
-     * @return StraightFlush
-     */
-    public function newHand(Hand $Hand) {
-        $SpadesCards = array();
-        $HeartsCards = array();
-        $ClubsCards = array();
-        $DiamondsCards = array();
+    public override StraightFlush newHand(Hand Hand) {
+        var SpadesCards = new List<Card>();
+        var HeartsCards = new List<Card>();
+        var ClubsCards = new List<Card>();
+        var DiamondsCards = new List<Card>();
 
-        $Cards = $Hand->getCards();
+        var Cards = Hand.getCards().Sort((Card1, Card2) => { return Card1.compareTo(Card2); });
 
-        usort(
-            $Cards,
-            function (Card $Card1, Card $Card2) {
-                return $Card1->compareTo($Card2);
-            }
-        );
-
-        foreach ($Cards as $Card) {
-            switch ($Card->getSuit()) {
+        foreach (var Card in Cards) {
+            switch (Card.getSuit()) {
                 case Suit::SPADES:
-                    if ($this->_canAddCardTo($SpadesCards, $Card)) {
-                        $SpadesCards[] = $Card;
-                        if (count($SpadesCards) == 5) {
-                            return new StraightFlush($SpadesCards);
+                    if (canAddCardTo(SpadesCards, Card)) {
+                        SpadesCards.Add(Card);
+                        if (count(SpadesCards) == 5) {
+                            return new StraightFlush(SpadesCards);
                         }
                     }
                     break;
 
                 case Suit::HEARTS:
-                    if ($this->_canAddCardTo($HeartsCards, $Card)) {
-                        $HeartsCards[] = $Card;
-                        if (count($HeartsCards) == 5) {
-                            return new StraightFlush($HeartsCards);
+                    if (canAddCardTo(HeartsCards, Card)) {
+                        HeartsCards.Add(Card);
+                        if (count(HeartsCards) == 5) {
+                            return new StraightFlush(HeartsCards);
                         }
                     }
                     break;
 
                 case Suit::CLUBS:
-                    if ($this->_canAddCardTo($ClubsCards, $Card)) {
-                        $ClubsCards[] = $Card;
-                        if (count($ClubsCards) == 5) {
-                            return new StraightFlush($ClubsCards);
+                    if (canAddCardTo(ClubsCards, Card)) {
+                        ClubsCards.Add(Card);
+                        if (count(ClubsCards) == 5) {
+                            return new StraightFlush(ClubsCards);
                         }
                     }
                     break;
 
                 case Suit::DIAMONDS:
-                    if ($this->_canAddCardTo($DiamondsCards, $Card)) {
-                        $DiamondsCards[] = $Card;
-                        if (count($DiamondsCards) == 5) {
-                            return new StraightFlush($DiamondsCards);
+                    if (canAddCardTo(DiamondsCards, Card)) {
+                        DiamondsCards.Add(Card);
+                        if (count(DiamondsCards) == 5) {
+                            return new StraightFlush(DiamondsCards);
                         }
                     }
                     break;
@@ -72,20 +55,15 @@ class StraightFlushSpecification extends HandSpecification {
         return null;
     }
 
-    /**
-     * @param Card[] $Cards
-     * @param Card $Card
-     * @return boolean
-     */
-    private function _canAddCardTo($Cards, Card $Card) {
-        $numCards = count($Cards);
+    private bool canAddCardTo(List<Card> Cards, Card Card) {
+        numCards = Cards.Count;
 
-        if ($numCards > 0) {
-            $PreviousCard = $Cards[$numCards - 1];
+        if (numCards > 0) {
+            PreviousCard = Cards.toArray()[numCards - 1];
 
-            if ($PreviousCard->isAce() && $Card->getFaceValue() == 5) {
+            if (PreviousCard.isAce() && Card.getFaceValue() == 5) {
                 return true;
-            } else if ($PreviousCard->getFaceValue() - 1 == $Card->getFaceValue()) {
+            } else if (PreviousCard.getFaceValue() - 1 == Card.getFaceValue()) {
                 return true;
             }  else {
                 return false;
