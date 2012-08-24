66fd608da43e6d1e7cd27136af7a93dad591e8c8
Daniel Miladinov <daniel.miladinov@gmail.com>
2012-08-24 14:03:27 -0400

    Convert FullHouseSpecifiction from php to C#.
    Too much array/hash gymnastics happening here, let's get a working build
    with failing tests first, then I'll re-implement it in C#.

--- HandSpecifications/FullHouseSpecification.cs
+++ HandSpecifications/FullHouseSpecification.cs
@@ -1,58 +1,12 @@
-<?php
+using System.Collections.Generic;
 
-class FullHouseSpecification extends HandSpecification {
+class FullHouseSpecification : HandSpecification {
 
-    /**
-     * @param Hand $Hand
-     * @return boolean
-     */
-    public function isSatisfiedBy(Hand $Hand) {
-        return $this->newHand($Hand) instanceof FullHouse;
+    public override function isSatisfiedBy(Hand Hand) {
+        return newHand(Hand) is FullHouse;
     }
 
-    /**
-     * @param Hand $Hand
-     * @return FullHouse
-     */
-    public function newHand(Hand $Hand) {
-        $HandCards = array();
-        $GroupedByValue = $Hand->getCardsGroupedByValues();
-        $faceValueCounts = array();
-
-        foreach ($GroupedByValue as $faceValue => $CardsWithSameValue) {
-            $faceValueCount = count($CardsWithSameValue);
-
-            if ($faceValueCount >= 2) {
-                $faceValueCounts[$faceValue] = $faceValueCount;
-            }
-        }
-
-        if (count($faceValueCounts) >= 2) {
-            krsort($faceValueCounts);
-
-            list($faceValue, $faceValueCount) = each($faceValueCounts);
-
-            if ($faceValueCount == 3) {
-                $HandCards = array_merge($HandCards, $GroupedByValue[$faceValue]);
-
-                list($nextFaceValue, $nextFaceValueCount) = each($faceValueCounts);
-
-                if ($nextFaceValueCount >= 2) {
-                    $HandCards = array_merge($HandCards, array_slice($GroupedByValue[$nextFaceValue], 0, 2));
-                    return new FullHouse($HandCards);
-                }
-            } else if ($faceValueCount == 2) {
-                $HandCards = array_merge($HandCards, $GroupedByValue[$faceValue]);
-
-                list($nextFaceValue, $nextFaceValueCount) = each($faceValueCounts);
-
-                if ($nextFaceValueCount == 3) {
-                    $HandCards = array_merge($HandCards, array_slice($GroupedByValue[$nextFaceValue], 0, 3));
-                    return new FullHouse($HandCards);
-                }
-            }
-        }
-
+    public override FullHouse newHand(Hand Hand) {
         return null;
     }
 }
