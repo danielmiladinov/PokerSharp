8e825d4411e2c61a6d458d631785a47f7070ddb6
Daniel Miladinov <daniel.miladinov@gmail.com>
2012-08-24 16:09:33 -0400

    Convert TwoPairSpecification from php to C#.

--- HandSpecifications/TwoPairSpecification.cs
+++ HandSpecifications/TwoPairSpecification.cs
@@ -1,29 +1,19 @@
-<?php
+class TwoPairSpecification : HandSpecification {
 
-class TwoPairSpecification extends HandSpecification {
-
-    /**
-     * @param Hand $Hand
-     * @return boolean
-     */
-    public function isSatisfiedBy(Hand $Hand) {
-        $numPairsSeen = 0;
-        $CardsGroupedByValues = $Hand->getCardsGroupedByValues();
-        foreach ($CardsGroupedByValues as $Cards) {
-            if (count($Cards) == 2) {
-                $numPairsSeen++;
+    public override bool isSatisfiedBy(Hand Hand) {
+        int numPairsSeen = 0;
+        var CardsGroupedByValues = Hand.getCardsGroupedByValues();
+        foreach (var Cards in CardsGroupedByValues) {
+            if (Cards.Count == 2) {
+                numPairsSeen++;
             }
         }
 
-        $canMakeTwoPair = (2 == $numPairsSeen);
-        return $canMakeTwoPair;
+        var canMakeTwoPair = (2 == numPairsSeen);
+        return canMakeTwoPair;
     }
 
-    /**
-     * @param $Hand
-     * @return TwoPair
-     */
-    public function newHand(Hand $Hand) {
-        return new TwoPair($Hand->getCards());
+    public override TwoPair newHand(Hand Hand) {
+        return new TwoPair(Hand.getCards());
     }
 }
