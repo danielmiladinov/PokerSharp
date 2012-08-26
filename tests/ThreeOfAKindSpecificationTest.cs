31ad47e00c9f6625f8753430ba52bfb47fc8e977
Daniel Miladinov <daniel.miladinov@gmail.com>
2012-08-25 21:52:00 -0400

    Get ThreeOfAKindSpecificationTest to compile.

--- tests/ThreeOfAKindSpecificationTest.cs
+++ tests/ThreeOfAKindSpecificationTest.cs
@@ -5,20 +5,20 @@ class ThreeOfAKindSpecificationTest : PokerTestCase {
 
     private ThreeOfAKindSpecification Specification;
 
-    [Setup]
+    [SetUp]
     protected void setUp() {
         Specification = new ThreeOfAKindSpecification();
     }
 
     [Test]
     public void shouldBeSatisfiedByAThreeOfAKind() {
-        Hand = new Hand(theFiveCardsAre("J-H", "J-C", "J-D", "3-S", "K-H"));
+        var Hand = new Hand(theFiveCardsAre("J-H", "J-C", "J-D", "3-S", "K-H"));
         Assert.IsTrue(Specification.isSatisfiedBy(Hand), "This is a valid ThreeOfAKind, why did it not satisfy the specification?");
     }
 
     [Test]
     public void shouldNotBeSatisfiedByATwoOfAKind() {
-        Hand = new Hand(theFiveCardsAre("J-H", "Q-C", "J-D", "3-S", "K-H"));
+        var Hand = new Hand(theFiveCardsAre("J-H", "Q-C", "J-D", "3-S", "K-H"));
         Assert.IsFalse(Specification.isSatisfiedBy(Hand), "This is not a ThreeOfAKind!");
     }
 }
