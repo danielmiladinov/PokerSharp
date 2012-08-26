39a8bc9c49e7cc9365eeace1505f00c2ebbda74f
Daniel Miladinov <daniel.miladinov@gmail.com>
2012-08-25 22:43:20 -0400

    Get FourOfAKindSpecification to compile.

--- HandSpecifications/FourOfAKindSpecification.cs
+++ HandSpecifications/FourOfAKindSpecification.cs
@@ -1,5 +1,5 @@
 class FourOfAKindSpecification : CardsOfAKindSpecification {
 
-    public FourOfAKindSpecification() : base(4, FourOfAKind) {
+    public FourOfAKindSpecification() : base(4, typeof(FourOfAKind)) {
     }
 }
