6f3038e676d12d3bbede061df95429152a6c9e43
Daniel Miladinov <daniel.miladinov@gmail.com>
2012-08-25 22:15:40 -0400

    Get Deck to compile.

--- Deck.cs
+++ Deck.cs
@@ -2,50 +2,50 @@ using System.Collections.Generic;
 
 class Deck {
 
-    private List<Card> Cards;
+    private List<Card> cards;
 
     public Deck() {
-        Cards = new List<Card>();
+        cards = new List<Card>();
     }
 
     public void add(Card Card) {
-        if (Cards.Contains(Card)) {
+        if (cards.Contains(Card)) {
             throw new DeckIntegrityException("Cannot contain the same card twice!");
         } else {
-            Cards.Add(Card);
+            cards.Add(Card);
         }
     }
 
     public int size() {
-        return Cards.Length;
+        return cards.Count;
     }
 
     public void populate() {
         var Suits = new Suit[] {
-            Suit::Spades(),
-            Suit::Hearts(),
-            Suit::Clubs(),
-            Suit::Diamonds(),
+            Suit.Spades(),
+            Suit.Hearts(),
+            Suit.Clubs(),
+            Suit.Diamonds(),
         };
 
-        foreach (var Suit in Suits) {
-            Cards.Add(Cards::aceOf(Suit));
-            Cards.Add(Cards::twoOf(Suit));
-            Cards.Add(Cards::threeOf(Suit));
-            Cards.Add(Cards::fourOf(Suit));
-            Cards.Add(Cards::fiveOf(Suit));
-            Cards.Add(Cards::sixOf(Suit));
-            Cards.Add(Cards::sevenOf(Suit));
-            Cards.Add(Cards::eightOf(Suit));
-            Cards.Add(Cards::nineOf(Suit));
-            Cards.Add(Cards::tenOf(Suit));
-            Cards.Add(Cards::jackOf(Suit));
-            Cards.Add(Cards::queenOf(Suit));
-            Cards.Add(Cards::kingOf(Suit));
+        foreach (var suit in Suits) {
+            cards.Add(Cards.aceOf(suit));
+            cards.Add(Cards.twoOf(suit));
+            cards.Add(Cards.threeOf(suit));
+            cards.Add(Cards.fourOf(suit));
+            cards.Add(Cards.fiveOf(suit));
+            cards.Add(Cards.sixOf(suit));
+            cards.Add(Cards.sevenOf(suit));
+            cards.Add(Cards.eightOf(suit));
+            cards.Add(Cards.nineOf(suit));
+            cards.Add(Cards.tenOf(suit));
+            cards.Add(Cards.jackOf(suit));
+            cards.Add(Cards.queenOf(suit));
+            cards.Add(Cards.kingOf(suit));
         }
     }
 
     public Card[] getCards() {
-        return Cards.ToArray();
+        return cards.ToArray();
     }
 }
