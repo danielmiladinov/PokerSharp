cdd5c8e5cf1a6c860851eeea95b8e31902b0e6c7
Daniel Miladinov <daniel.miladinov@gmail.com>
2012-08-25 21:53:05 -0400

    Get TwoPairSpecificationTest to compile.

--- tests/TwoPairSpecificationTest.cs
+++ tests/TwoPairSpecificationTest.cs
@@ -5,20 +5,20 @@ class TwoPairSpecificationTest : PokerTestCase {
 
     private TwoPairSpecification Specification;
 
-    [Setup]
+    [SetUp]
     protected void setUp() {
         Specification = new TwoPairSpecification();
     }
 
     [Test]
     public void shouldBeSatisfiedByATwoPair() {
-        Hand = new Hand(theFiveCardsAre("A-S", "9-C", "9-D", "3-C", "3-H"));
+        var Hand = new Hand(theFiveCardsAre("A-S", "9-C", "9-D", "3-C", "3-H"));
         Assert.IsTrue(Specification.isSatisfiedBy(Hand), "This is a valid TwoPair, why did it not satisfy the specification?");
     }
 
     [Test]
     public void shouldNotBeSatisfiedByAFullHouse() {
-        Hand = new Hand(theFiveCardsAre("3-H", "3-D", "3-S", "K-C", "K-D"));
+        var Hand = new Hand(theFiveCardsAre("3-H", "3-D", "3-S", "K-C", "K-D"));
         Assert.IsFalse(Specification.isSatisfiedBy(Hand), "This is not a valid TwoPair!");
     }
 }
