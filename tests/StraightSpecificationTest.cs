d9c01673bd13e634ae42da7164ef7c5ffdc7d54e
Daniel Miladinov <daniel.miladinov@gmail.com>
2012-08-25 21:51:30 -0400

    Get StraightSpecificationTest to compile.

--- tests/StraightSpecificationTest.cs
+++ tests/StraightSpecificationTest.cs
@@ -3,24 +3,23 @@ using NUnit.Framework;
 [TestFixture]
 class StraightSpecificationTest : PokerTestCase {
 
-    [Test]
     private StraightSpecification Specification;
 
-    [Setup]
+    [SetUp]
     protected void setUp() {
         Specification = new StraightSpecification();
     }
 
     [Test]
     public void shouldBeSatisfiedByAStraight() {
-        Hand = new Hand(theFiveCardsAre("2-H", "3-C", "4-D", "5-H", "6-S"));
-        Assert.IsTrue(Specification->isSatisfiedBy(Hand), "This is a valid Straight, why did it not satisfy the specification?");
+        var Hand = new Hand(theFiveCardsAre("2-H", "3-C", "4-D", "5-H", "6-S"));
+        Assert.IsTrue(Specification.isSatisfiedBy(Hand), "This is a valid Straight, why did it not satisfy the specification?");
     }
 
     [Test, TestCaseSource("SomeHandsThatAreNotStraights")]
     public void shouldNotBeSatisfiedByHandsThatAreNotStraights(string card1, string card2, string card3, string card4, string card5) {
-        Hand = new Hand(theFiveCardsAre(card1, card2, card3, card4, card5));
-        Assert.IsFalse(Specification->isSatisfiedBy(Hand, "That is not a Straight! ({0}, {1}, {2}, {3}, {4})", card1, card2, card3, card4, card5));
+        var Hand = new Hand(theFiveCardsAre(card1, card2, card3, card4, card5));
+        Assert.IsFalse(Specification.isSatisfiedBy(Hand), "That is not a Straight! ({0}, {1}, {2}, {3}, {4})", card1, card2, card3, card4, card5);
     }
 
     static object[][] SomeHandsThatAreNotStraights = {
