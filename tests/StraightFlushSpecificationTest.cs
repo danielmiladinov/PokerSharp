302482d04a2b33d324f193c1203b5ec94084b53c
Daniel Miladinov <daniel.miladinov@gmail.com>
2012-08-25 21:49:43 -0400

    Get StraightFlushSpecificationTest to compile.

--- tests/StraightFlushSpecificationTest.cs
+++ tests/StraightFlushSpecificationTest.cs
@@ -5,26 +5,26 @@ class StraightFlushSpecificationTest : PokerTestCase {
 
     private StraightFlushSpecification Specification;
 
-    [Setup]
+    [SetUp]
     protected void setUp() {
         Specification = new StraightFlushSpecification();
     }
 
     [Test]
     public void shouldBeSatisfiedByAHeartsStraightFlush() {
-        Hand = new Hand(theFiveCardsAre("Q-H", "J-H", "10-H", "9-H", "8-H"));
+        var Hand = new Hand(theFiveCardsAre("Q-H", "J-H", "10-H", "9-H", "8-H"));
         Assert.IsTrue(Specification.isSatisfiedBy(Hand), "This is a valid StraightFlush, why did it not satisfy the specification?");
     }
 
     [Test]
     public void shouldNotBeSatisfiedByJustAFlush() {
-        Hand = new Hand(theFiveCardsAre("Q-C", "6-C", "10-C", "9-C", "8-C"));
+        var Hand = new Hand(theFiveCardsAre("Q-C", "6-C", "10-C", "9-C", "8-C"));
         Assert.IsFalse(Specification.isSatisfiedBy(Hand), "Not just any Flush can be a StraightFlush!");
     }
 
     [Test]
     public void shouldNotBeSatisfiedByJustAStraight() {
-        Hand = new Hand(theFiveCardsAre("Q-H", "J-C", "10-D", "9-S", "8-H"));
+        var Hand = new Hand(theFiveCardsAre("Q-H", "J-C", "10-D", "9-S", "8-H"));
         Assert.IsFalse(Specification.isSatisfiedBy(Hand), "Not just any Straight can be a StraightFlush!");
     }
 }
