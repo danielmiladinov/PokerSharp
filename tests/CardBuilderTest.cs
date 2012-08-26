ee1c207edebf4f93a50253bb73ed6d162daef820
Daniel Miladinov <daniel.miladinov@gmail.com>
2012-08-26 00:36:29 -0400

    Return to CardBuilderTest's original intent.
    I wasn't able to get any kind of equality assertion to work on the
    expected and actual card objects, regardless of the fact that they were
    in fact the same card values, as far as their face value and suit were
    concerned.  That being the case, why try to compare anything else besides
    their ToString() contents?

--- tests/CardBuilderTest.cs
+++ tests/CardBuilderTest.cs
@@ -44,49 +44,44 @@ class CardBuilderTest {
     public void willGetACardWithValidParameters() {
         var validString = "2-C";
         var ExpectedCard = new Clubs(2);
-
         var ActualCard = CardBuilder.fromString(validString);
 
-        Assert.AreEqual(ExpectedCard, ActualCard);
+        Assert.AreEqual(ExpectedCard.ToString(), ActualCard.ToString());
     }
 
     [Test]
     public void willGetACardWithFaceCard() {
         var validString = "Q-S";
         var ExpectedCard = new Spades(Card.QUEEN);
-
         var ActualCard = CardBuilder.fromString(validString);
 
-        Assert.AreEqual(ExpectedCard, ActualCard);
+        Assert.AreEqual(ExpectedCard.ToString(), ActualCard.ToString());
     }
 
     [Test]
     public void willGetACardWithJacks() {
         var validString = "J-H";
         var ExpectedCard = new Hearts(Card.JACK);
-
         var ActualCard = CardBuilder.fromString(validString);
 
-        Assert.AreEqual(ExpectedCard, ActualCard);
+        Assert.AreEqual(ExpectedCard.ToString(), ActualCard.ToString());
     }
 
     [Test]
     public void willGetACardWithKings() {
         var validString = "K-D";
         var ExpectedCard = new Diamonds(Card.KING);
-
         var ActualCard = CardBuilder.fromString(validString);
 
-        Assert.AreEqual(ExpectedCard, ActualCard);
+        Assert.AreEqual(ExpectedCard.ToString(), ActualCard.ToString());
     }
 
     [Test]
     public void willGetACardWithAce() {
         var validString = "A-C";
         var ExpectedCard = new Clubs(Card.ACE);
-
         var ActualCard = CardBuilder.fromString(validString);
 
-        Assert.AreEqual(ExpectedCard, ActualCard);
+        Assert.AreEqual(ExpectedCard.ToString(), ActualCard.ToString());
     }
 }
