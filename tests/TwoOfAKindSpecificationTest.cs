4bd5622f250bcb150d984b05e912466693eabbe6
Daniel Miladinov <daniel.miladinov@gmail.com>
2012-08-25 21:52:53 -0400

    Get TwoOfAKindSpecificationTest to compile.

--- tests/TwoOfAKindSpecificationTest.cs
+++ tests/TwoOfAKindSpecificationTest.cs
@@ -5,32 +5,32 @@ class TwoOfAKindSpecificationTest : PokerTestCase {
 
     private TwoOfAKindSpecification Specification;
 
-    [Setup]
+    [SetUp]
     protected void setUp() {
         Specification = new TwoOfAKindSpecification();
     }
 
     [Test]
     public void shouldBeSatisfiedByATwoOfAKind() {
-        Hand = new Hand(theFiveCardsAre("2-C", "3-C", "4-C", "5-C", "4-H"));
+        var Hand = new Hand(theFiveCardsAre("2-C", "3-C", "4-C", "5-C", "4-H"));
         Assert.IsTrue(Specification.isSatisfiedBy(Hand), "This is a valid TwoOfAKind, why did it not satisfy the specification?");
     }
 
     [Test]
     public void shouldNotBeSatisfiedByRandomJunkCards() {
-        Hand = new Hand(theFiveCardsAre("K-H", "10-S", "3-C", "5-D", "7-S"));
+        var Hand = new Hand(theFiveCardsAre("K-H", "10-S", "3-C", "5-D", "7-S"));
         Assert.IsFalse(Specification.isSatisfiedBy(Hand), "This is not a valid TwoOfAKind!");
     }
 
     [Test]
     public void shouldNotBeSatisfiedByAThreeOfAKind() {
-        Hand = new Hand(theFiveCardsAre("K-H", "K-S", "K-C", "5-D", "7-S"));
+        var Hand = new Hand(theFiveCardsAre("K-H", "K-S", "K-C", "5-D", "7-S"));
         Assert.IsFalse(Specification.isSatisfiedBy(Hand), "This is not a valid TwoOfAKind!");
     }
 
     [Test]
     public void shouldNotBeSatisfiedByAFourOfAKind() {
-        Hand = new Hand(theFiveCardsAre("K-H", "K-S", "K-C", "K-D", "7-S"));
+        var Hand = new Hand(theFiveCardsAre("K-H", "K-S", "K-C", "K-D", "7-S"));
         Assert.IsFalse(Specification.isSatisfiedBy(Hand), "This is not a valid TwoOfAKind!");
     }
 }
