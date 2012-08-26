f22c9bc8878d57db691cc23dd69ab66e7e0911f5
Daniel Miladinov <daniel.miladinov@gmail.com>
2012-08-25 21:48:36 -0400

    Get RoyalFlushSpecificationTest to compile.

--- tests/RoyalFlushSpecificationTest.cs
+++ tests/RoyalFlushSpecificationTest.cs
@@ -5,32 +5,32 @@ class RoyalFlushSpecificationTest : PokerTestCase {
 
     private RoyalFlushSpecification Specification;
 
-    [Setup]
+    [SetUp]
     protected void setUp() {
         Specification = new RoyalFlushSpecification();
     }
 
     [Test]
     public void shouldBeSatisfiedByASpadesRoyalFlush() {
-        Hand = new Hand(theFiveCardsAre("A-S", "K-S", "Q-S", "J-S", "10-S"));
+        var Hand = new Hand(theFiveCardsAre("A-S", "K-S", "Q-S", "J-S", "10-S"));
         Assert.IsTrue(Specification.isSatisfiedBy(Hand), "This is a valid RoyalFlush, why did it not satisfy the specification?");
     }
 
     [Test]
     public void shouldNotBeSatisfiedByAStraightFlush() {
-        Hand = new Hand(theFiveCardsAre("K-S", "Q-S", "J-S", "10-S", "9-S"));
+        var Hand = new Hand(theFiveCardsAre("K-S", "Q-S", "J-S", "10-S", "9-S"));
         Assert.IsFalse(Specification.isSatisfiedBy(Hand), "Not just any StraightFlush can be a RoyalFlush!");
     }
 
     [Test]
     public void shouldNotBeSatisfiedByAnAceHighCard() {
-        Hand = new Hand(theFiveCardsAre("A-H", "2-S", "3-C", "J-D", "7-D"));
+        var Hand = new Hand(theFiveCardsAre("A-H", "2-S", "3-C", "J-D", "7-D"));
         Assert.IsFalse(Specification.isSatisfiedBy(Hand), "Not just any Ace HighCard can be a RoyalFlush!");
     }
 
     [Test]
     public void shouldNotBeSatisfiedByAnAceHighNonFlushStraight() {
-        Hand = new Hand(theFiveCardsAre("A-H", "K-H", "Q-C", "J-H", "10-H"));
+        var Hand = new Hand(theFiveCardsAre("A-H", "K-H", "Q-C", "J-H", "10-H"));
         Assert.IsFalse(Specification.isSatisfiedBy(Hand), "Not just any Ace high Straight can be a RoyalFlush!");
     }
 }
