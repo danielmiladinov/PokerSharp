e237c2b6a79f7837c53c154e0f6e669ceade67e7
Daniel Miladinov <daniel.miladinov@gmail.com>
2012-08-26 11:28:00 -0400

    Add the long-awaited FullHouseSpecification::newHand().
    It's still modeled after the php version's algorithm, which was much
    easier to convert now after more exposure to how C# works, namely the
    collections, generics, and LINQ's extension methods.

--- HandSpecifications/FullHouseSpecification.cs
+++ HandSpecifications/FullHouseSpecification.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Collections.Generic;
 
 class FullHouseSpecification : HandSpecification {
@@ -6,7 +7,51 @@ class FullHouseSpecification : HandSpecification {
         return newHand(Hand) is FullHouse;
     }
 
-    public override Hand newHand(Hand Hand) {
+    public override Hand newHand(Hand hand) {
+        var handCards = new List<Card>();
+        var groupedByValue = hand.getCardsGroupedByValues();
+        var faceValueCounts = new Dictionary<int, int>();
+
+        foreach (KeyValuePair<int, List<Card>> cardsOfValue in groupedByValue) {
+            var faceValue = cardsOfValue.Key;
+            var faceValueCount = cardsOfValue.Value.Count;
+
+            if (faceValueCount >= 2) {
+                faceValueCounts[faceValue] = faceValueCount;
+            }
+        }
+
+        if (faceValueCounts.Count >= 2) {
+            faceValueCounts.Keys.ToList().Sort((x, y) => { return y - x; });
+
+            var faceValue = faceValueCounts.First().Key;
+            var faceValueCount = faceValueCounts.First().Value;
+
+            if (faceValueCount == 3) {
+                handCards.AddRange(groupedByValue[faceValue]);
+                faceValueCounts.Remove(faceValue);
+
+                faceValue = faceValueCounts.First().Key;
+                faceValueCount = faceValueCounts.First().Value;
+
+                if (faceValueCount >= 2) {
+                    handCards.AddRange(groupedByValue[faceValue].Take(2));
+                    return new FullHouse(handCards);
+                }
+            } else if (faceValueCount == 2) {
+                handCards.AddRange(groupedByValue[faceValue]);
+                faceValueCounts.Remove(faceValue);
+
+                faceValue = faceValueCounts.First().Key;
+                faceValueCount = faceValueCounts.First().Value;
+
+                if (faceValueCount == 3) {
+                    handCards.AddRange(groupedByValue[faceValue].Take(3));
+                    return new FullHouse(handCards);
+                }
+            }
+        }
+
         return null;
     }
 }
